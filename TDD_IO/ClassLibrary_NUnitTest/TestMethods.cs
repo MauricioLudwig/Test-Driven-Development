7e5731a9eb26b01bde8ee703c746acaa9e37ba49
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-01-11 12:45:00 +0100

    TDD - Input/Output

---
+++ TDD_IO/ClassLibrary_NUnitTest/TestMethods.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TDD_IO;
+using System.IO;
+
+namespace ClassLibrary_NUnitTest
+{
+    [TestFixture]
+    public class TestMethods
+    {
+
+        Runtime runtime = new Runtime();
+
+        [Test]
+        public void ValidateFile()
+        {
+            bool expectedResult = true;
+
+            string textFile = "MyTextFile.txt";
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string textFilePath = directory + "\\" + textFile;
+
+            bool actualResult = File.Exists(textFilePath);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ValidateName(
+            [Values("Franz", "Damien")] string firstName,
+            [Values("Kafka", "Gallagher")] string lastName)
+        {
+            string expectedResult = firstName + " " + lastName;
+            string actualResult = runtime.NameConcatination(firstName, lastName);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ValidateAddition(
+            [Values(1, 0)] int x,
+            [Values(2, 7)] int y)
+        {
+            int expectedResult = x + y;
+            int actualResult = runtime.Add(x, y);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ValidateSubstraction(
+            [Values(-1, 10)] int x,
+            [Values(5, 60)] int y)
+        {
+            int expectedResult = x - y;
+            int actualResult = runtime.Substract(x, y);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ValidateMultiplication(
+            [Values(-10, 6)] int x,
+            [Values(0, 22)] int y)
+        {
+            int expectedResult = x * y;
+            int actualResult = runtime.Multiply(x, y);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ValidateDivision(
+            [Values(0, 1)] int x,
+            [Values(2 ,1)] int y)
+        {
+            int expectedResult = x / y;
+            int? actualResult = runtime.Divide(x, y);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void DivisionByZero()
+        {
+            int x = 1;
+            int y = 0;
+            int? expectedResult = null;
+            int? actualResult = runtime.Divide(x, y);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void ValidateReadAndWrite(
+            [Values(8, 2)] int x,
+            [Values(0, -5)] int y,
+            [Values("Damien", "Franz")] string firstName,
+            [Values("Gallagher", "Kafka")] string lastName)
+        {
+
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string pathway = directory + "\\" + "TextFile_Test.txt";
+
+            string[] expectedResult = new string[]
+            {
+                runtime.NameConcatination(firstName, lastName),
+                runtime.Add(x, y).ToString(),
+                runtime.Substract(x, y).ToString(),
+                runtime.Multiply(x, y).ToString(),
+                runtime.Divide(x, y).ToString(),
+            };
+
+            runtime.WriteToTextFile(expectedResult, pathway);
+
+            string[] actualResult = File.ReadAllLines(pathway);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.AreEqual(actualResult[i], expectedResult[i]);
+            }
+        }
+
+    }
+}
---
+++ TDD_IO/TDD_IO/Runtime.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TDD_IO
+{
+    public class Runtime
+    {
+        private string firstName, lastName;
+        private int num1, num2;
+        private string textFilePath;
+
+        public Runtime()
+        {
+            string textFile = "MyTextFile.txt";
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            textFilePath = directory + "\\" + textFile;
+            if (!(File.Exists(textFilePath)))
+                File.Create(textFilePath);
+        }
+
+        public void Start()
+        {
+            GetValuesFromUser();
+
+            string[] textFileArray = new string[]
+            {
+                NameConcatination(firstName, lastName),
+                Add(num1, num2).ToString(),
+                Substract(num1, num2).ToString(),
+                Multiply(num1, num2).ToString(),
+                Divide(num1, num2).ToString(),
+            };
+
+            WriteToTextFile(textFileArray, textFilePath);
+        }
+
+        public void GetValuesFromUser()
+        {
+            Console.WriteLine("Enter your first name: ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name: ");
+            lastName = Console.ReadLine();
+
+            num1 = ValidInteger("Enter first number: ");
+            num2 = ValidInteger("Enter second number: ");
+        }
+
+        public void WriteToTextFile(string[] textFileArray, string pathway)
+        {
+            File.WriteAllLines(pathway, textFileArray);
+        }
+
+        public int ValidInteger(string text)
+        {
+            bool validInput;
+            string input;
+            int output;
+
+            while (true)
+            {
+                Console.WriteLine(text);
+                input = Console.ReadLine();
+                validInput = int.TryParse(input, out output);
+
+                if (validInput)
+                    return output;
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public string NameConcatination(string x, string y)
+        {
+            // return "Damien Gallagher";
+            return String.Format(x + " " + y);
+        }
+
+        public int Add(int x, int y)
+        {
+            // return 3;
+            return x + y;
+        }
+
+        public int Substract(int x, int y)
+        {
+            // return -6;
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            // return 0;
+            return x * y;
+        }
+
+        public int? Divide(int x, int y)
+        {
+            // return 0
+            if (!(y == 0))
+                return x / y;
+            else
+                // return null
+                return null;
+        }
+    }
+}
