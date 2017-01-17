c4ee99dbc564c7b455766da728d978359dd8917f
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-01-18 00:01:06 +0100

    Roman Numeral Converter added.

---
+++ NumberSystemConverter/NumberSystemConverter/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSystemConverter
+{
+    public class Program
+    {
+
+        static RomanNumeralConverter converter;
+
+        static void Main(string[] args)
+        {
+            converter = new RomanNumeralConverter();
+            Run();
+        }
+        
+        public static void Run()
+        {
+
+            // Instance variables
+            string stringInput;
+            bool validInput;
+            int integerInput;
+            string result;
+
+            while (true)
+            {
+                #region Menu
+                Console.WriteLine("Numeral Registry");
+                Console.WriteLine("M  - 1000");
+                Console.WriteLine("D  - 500");
+                Console.WriteLine("C  - 100");
+                Console.WriteLine("L  - 50");
+                Console.WriteLine("X  - 10");
+                Console.WriteLine("IX - 9");
+                Console.WriteLine("V  - 5");
+                Console.WriteLine("IV - 4");
+                Console.WriteLine("I  - 1\n");
+                Console.WriteLine("Please enter an integer value OR roman numeral to be converted...");
+                #endregion
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\nInput: ");
+                Console.ResetColor();
+
+                stringInput = Console.ReadLine().ToUpper();
+                validInput = int.TryParse(stringInput, out integerInput);
+
+                if (!(String.IsNullOrEmpty(stringInput)))
+                {
+
+                    if (validInput)
+                    {
+                        // if string is a valid integer input
+                        result = converter.ConvertToRomanNumeral(integerInput);
+                    } 
+                    else
+                    {
+                        // if string is not a valid integer input
+                        // if string is defined in the Roman Numeral enum list
+                        if (stringInput.All(c => Enum.IsDefined(typeof(RomanNumeralsType), c.ToString())))
+                        {
+                            result = converter.ConvertToInteger(stringInput).ToString();
+                        }
+                        // if string is not defined in the Roman Numeral enum list
+                        else
+                            result = String.Format("Invalid input, try again...");
+                    }
+
+                    Console.WriteLine("{0} = {1}", stringInput, result);
+                }
+                else
+                    Console.WriteLine("No empty strings are allowed.");
+
+                Console.Write("\nPress any key...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ NumberSystemConverter/RomanNumeralConverter/RomanNumeralConverter.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace NumberSystemConverter
+{
+    #region Container for allowing management of RomanNumeralsType enumerals
+    struct RomanNumeralPair
+    {
+        public int NumericValue { get; set; }
+        public string RomanNumeralRepresentation { get; set; }
+    }
+    #endregion
+    #region Roman Numerals - Seven symbols(enum RomanNumeralsType) with respective value.
+    public enum RomanNumeralsType
+    {
+        M = 1000,
+        D = 500,
+        C = 100,
+        L = 50,
+        X = 10,
+        IX = 9,
+        V = 5,
+        IV = 4,
+        I = 1
+    }
+    #endregion
+    public class RomanNumeralConverter
+    {
+        // Readonly - The variable assigned with the readonly operator can only be changed inside the declaration or in the constructor
+        private List<RomanNumeralPair> romanNumeralList;
+
+        public RomanNumeralConverter()
+        {
+            #region Initializing the list with romanNumerals
+            romanNumeralList = new List<RomanNumeralPair>()
+            {
+                new RomanNumeralPair() {
+                    // 1000
+                    NumericValue = (int)RomanNumeralsType.M,
+                    RomanNumeralRepresentation = RomanNumeralsType.M.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 500
+                    NumericValue = (int)RomanNumeralsType.D,
+                    RomanNumeralRepresentation = RomanNumeralsType.D.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 100
+                    NumericValue = (int)RomanNumeralsType.C,
+                    RomanNumeralRepresentation = RomanNumeralsType.C.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 50
+                    NumericValue = (int)RomanNumeralsType.L,
+                    RomanNumeralRepresentation = RomanNumeralsType.L.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 10
+                    NumericValue = (int)RomanNumeralsType.X,
+                    RomanNumeralRepresentation = RomanNumeralsType.X.ToString()
+                },
+                new RomanNumeralPair()
+                {
+                    // 9
+                    NumericValue = (int)RomanNumeralsType.IX,
+                    RomanNumeralRepresentation = RomanNumeralsType.IX.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 5
+                    NumericValue = (int)RomanNumeralsType.V,
+                    RomanNumeralRepresentation = RomanNumeralsType.V.ToString()
+                },
+                new RomanNumeralPair()
+                {
+                    // 4
+                    NumericValue = (int)RomanNumeralsType.IV,
+                    RomanNumeralRepresentation = RomanNumeralsType.IV.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 1
+                    NumericValue = (int)RomanNumeralsType.I,
+                    RomanNumeralRepresentation = RomanNumeralsType.I.ToString()
+                }
+            };
+            #endregion 
+        }
+
+        public string ConvertToRomanNumeral(int number)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if (number == 0)
+                return "nulla";
+            else
+            {
+                // Iterate through the list, starting with the highest value
+                foreach (var currentPair in romanNumeralList)
+                {
+                    while (number >= currentPair.NumericValue)
+                    {
+                        // ...number is greater than or equal to the current value so store the roman numeral and decrement it's value
+                        builder.Append(currentPair.RomanNumeralRepresentation);
+                        number -= currentPair.NumericValue;
+                    }
+                }
+                return builder.ToString();
+            }
+        }
+
+        public int ConvertToInteger(string word)
+        {
+            List<int> digits = new List<int>();
+            int sum = 0;
+            int previousDigit = 0;
+
+            foreach (var character in word)
+            {
+                digits.Add((int)Enum.Parse(typeof(RomanNumeralsType), character.ToString()));
+            }
+
+            digits.Reverse();
+
+            sum += digits[0];
+
+            foreach (var digit in digits.Skip(1))
+            {
+                if (digit < sum && digit != previousDigit)
+                    sum -= digit;
+                else
+                    sum += digit;
+                previousDigit = digit;
+            }
+
+            return sum;
+        }
+    }
+}
